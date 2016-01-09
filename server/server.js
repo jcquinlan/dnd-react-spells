c60a6ea59eaa44359259bdc513dd1a707e490fd7
James <quinlanjc@vcu.edu>
2016-01-08 22:47:45 -0500

    add spells json to mongo

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
 Meteor.startup(function(){
-    const spells = JSON.parse(Assets.getText('test.json'));
-
     if( Spells.find().count() === 0){
         spells.forEach(function(spell){
             Spells.insert(spell);
         });
-        console.log(spells);
     }
 });
 
