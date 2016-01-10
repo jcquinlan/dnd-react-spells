52413135c0f075cbe3f008e2094334932acd4c81
James <quinlanjc@vcu.edu>
2016-01-10 10:18:12 -0500

    complete filter functionality

--- client/client.jsx
+++ client/client.jsx
@@ -8,6 +8,11 @@ const Main = React.createClass({
     render(){
         return (
             <div className="container">
+                <div className="row">
+                    <div className="col l12 center-align">
+                        <h1>D&D Spells</h1>
+                    </div>
+                </div>
                 <List />
             </div>
         )
@@ -15,22 +20,50 @@ const Main = React.createClass({
 });
 
 const List = React.createClass({
+    getInitialState(){
+        return {filter: 'Bless'};
+    },
+    render(){
+        return (
+            <div>
+                <FilterButtons updateFilter={this.updateFilter}/>
+                <SpellList filter={this.state.filter}/>
+            </div>
+        )
+    },
+    updateFilter(filter){
+        this.setState({ filter: filter });
+    }
+});
+
+
+
+
+const SpellList = React.createClass({
     mixins: [ReactMeteorData],
     render(){
         return (
 	    <div className="row">
-	    	<div className="col s8 offset-s2">
+	    	<div className="col l8 offset-l2">
                     <ul className="collection">
                         {this.renderSpells()}
                     </ul>
-		</div>
+		    </div>
 	    </div>
         )
     },
     getMeteorData(){
-        return {
-            spells: Spells.find().fetch()
+        let filter = this.props.filter;
+        if(filter === "All") {
+            return {
+                spells: Spells.find().fetch()
+            }
+        } else {
+            return {
+                spells: Spells.find({ name: this.props.filter }).fetch()
+            }
         }
+
     },
     renderSpells(){
         return this.data.spells.map((spell) => {
@@ -39,10 +72,39 @@ const List = React.createClass({
     }
 });
 
+
+
+
 const Spell = React.createClass({
     render(){
         return (
             <li className="collection-item"><div>{this.props.name} <div className="secondary-content">{this.props.level}</div></div></li>
         )
     }
-})
+});
+
+
+
+
+const FilterButtons = React.createClass({
+    render(){
+        return (
+            <div>
+                <form onChange={this.handleChange}>
+                    <p>
+                      <input name="spell-filter" type="radio" id="show-all" value="All"/>
+                      <label htmlFor="show-all">All</label>
+                    </p>
+                    <p>
+                      <input name="spell-filter" type="radio" id="show-aid" value="Aid"/>
+                      <label htmlFor="show-aid">Show Aid</label>
+                    </p>
+                </form>
+            </div>
+        )
+    },
+    handleChange(event){
+        let value = event.target.value;
+        this.props.updateFilter(value);
+    }
+});
