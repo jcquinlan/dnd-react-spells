515b499a2501ac205b2572bbdd91b2c24094d0f7
James <quinlanjc@vcu.edu>
2016-01-17 15:07:19 -0500

    add select form for filters

--- client/client.jsx
+++ client/client.jsx
@@ -19,13 +19,16 @@ const Main = React.createClass({
     }
 });
 
+
+
 const List = React.createClass({
     getInitialState(){
-        return {filter: 'Bless'};
+        return {filter: 'All'};
     },
     render(){
         return (
             <div>
+                <h3>Filter: {this.state.filter.split(', ')[0]}</h3>
                 <FilterButtons updateFilter={this.updateFilter}/>
                 <SpellList filter={this.state.filter}/>
             </div>
@@ -44,7 +47,7 @@ const SpellList = React.createClass({
     render(){
         return (
 	    <div className="row">
-	    	<div className="col l8 offset-l2">
+	    	<div className="col s8 offset-s2">
                     <ul className="collection">
                         {this.renderSpells()}
                     </ul>
@@ -53,14 +56,19 @@ const SpellList = React.createClass({
         )
     },
     getMeteorData(){
-        let filter = this.props.filter;
-        if(filter === "All") {
+        let filter = this.props.filter.split(', ');
+
+        if(filter[0] === "All") {
             return {
                 spells: Spells.find().fetch()
             }
-        } else {
+        } else if (filter[0] === "level") {
             return {
-                spells: Spells.find({ name: this.props.filter }).fetch()
+                spells: Spells.find({ level: filter[1] }).fetch()
+            }
+        } else if (filter[0] === "class") {
+            return {
+                spells: Spells.find({ class: filter[1] }).fetch()
             }
         }
 
@@ -90,16 +98,21 @@ const FilterButtons = React.createClass({
     render(){
         return (
             <div>
-                <form onChange={this.handleChange}>
-                    <p>
-                      <input name="spell-filter" type="radio" id="show-all" value="All"/>
-                      <label htmlFor="show-all">All</label>
-                    </p>
-                    <p>
-                      <input name="spell-filter" type="radio" id="show-aid" value="Aid"/>
-                      <label htmlFor="show-aid">Show Aid</label>
-                    </p>
-                </form>
+                <select onChange={this.handleChange} name="spell-filter" id="show-all">
+                  <option value="level, 1st-level">1st Level</option>
+                  <option value="level, 2nd-level">2nd Level</option>
+                  <option value="level, 3rd-level">3rd Level</option>
+                  <option value="level, 4th-level">4th Level</option>
+                </select>
+                <label htmlFor="show-all">Level</label>
+
+                <select onChange={this.handleChange} name="spell-filter" id="show-all">
+                  <option value="class, Bard">Bard</option>
+                  <option value="class, Warlock">Warlock</option>
+                  <option value="class, Fighter">Fighter</option>
+                  <option value="class, Wizard">Wizard</option>
+                </select>
+                <label htmlFor="show-all">Class</label>
             </div>
         )
     },
